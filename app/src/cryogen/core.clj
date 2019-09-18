bbcdc97d1d2bedba9a839f8fd0939cdb18816688
Harlan Iverson <h.iverson@gmail.com>
2019-09-18 12:40:28 -0700

    share cryogen init code so that one-time builds via run work

--- app/src/cryogen/core.clj
+++ app/src/cryogen/core.clj
@@ -1,8 +1,20 @@
 (ns cryogen.core
+  (:gen-class)
   (:require [cryogen-core.compiler :refer [compile-assets-timed]]
-            [cryogen-core.plugins :refer [load-plugins]]))
+            [cryogen-core.plugins :refer [load-plugins]]
+            [selmer.filters :as selmer-filters]))
+
+(defn selmer-some-tag? [x y]
+  (let [result (some (fn [tag]    (= (str (:name tag)) (str (read-string y))))
+                      x)]
+    [:safe (str (boolean result))]))
+
+
+(defn init-cryogen []
+  (selmer-filters/add-filter! :selmer-some-tag? selmer-some-tag?)
+  (load-plugins))
 
 (defn -main []
-  (load-plugins)
+  (init-cryogen)
   (compile-assets-timed)
   (System/exit 0))
--- app/src/cryogen/server.clj
+++ app/src/cryogen/server.clj
@@ -1,24 +1,29 @@
 (ns cryogen.server
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [GET POST defroutes]]
             [compojure.route :as route]
             [ring.util.response :refer [redirect file-response]]
             [ring.util.codec :refer [url-decode]]
+                        [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [cryogen-core.watcher :refer [start-watcher!]]
-            [cryogen-core.plugins :refer [load-plugins]]
             [cryogen-core.compiler :refer [compile-assets-timed]]
             [cryogen-core.config :refer [resolve-config]]
             [cryogen-core.io :refer [path]]
-            [selmer.filters :as selmer-filters]))
+            [selmer.filters :as selmer-filters]
+            ;[com.harlanji.authorize :as authorize]
+            [cryogen.core :as app-core]))
 
-(defn selmer-some-tag? [x y]
-  (let [result (some (fn [tag]    (= (str (:name tag)) (str (read-string y))))
-                      x)]
-    [:safe (str (boolean result))]))
 
 
 (defn init []
-  (load-plugins)
-  (selmer-filters/add-filter! :selmer-some-tag? selmer-some-tag?)
+  (app-core/init-cryogen)
+  ;(authorize/init "" "")
+  
+  ;#_ (let [payment-request (authorize/create-test-payment-request)]
+  ;  (println "Payment request: " payment-request)
+  ;  (let [result (authorize/send-payment-request payment-request)]
+  ;    (println "Success! Result: " result)))
+  
   (compile-assets-timed)
   (let [ignored-files (-> (resolve-config) :ignored-files)]
     (start-watcher! "content" ignored-files compile-assets-timed)
@@ -46,7 +51,51 @@
                           (path (:blog-prefix config)
                                 (when (= (:clean-urls config) :dirty)
                                   "index.html")))))
+  (GET "/login" [] (fn [req]
+    (let [the-password (System/getenv "HARLANJI_PASSWORD")
+          password (-> req :params :password)]
+      (println "The password: " the-password)
+      (println "Params: " (keys (:params req)))
+      
+      (if (and the-password (= the-password password))
+        {:status 200
+         :content-type "text/html"
+         :body "Hey. Login."}
+        {:status 401
+         :content-type "text/html"
+         :body "No login, buddy."}))))          
+                                  
+                                  
+  ;(POST "/authorize" [  ] (fn [req] ;; POST... we GET from the address bar. Test...
+  ;  ;; I am leaving this hardcoded because I am going to switch to Bidi. I don't care to learn Compojure. For the reason that
+  ;  ;; it is macro driven, which requires special syntax memory. too much. I prefer bidi.
+  ;  ;; As of 9/3/2019 it is deleted via console, so the first POST will succeed.    
+  ;  (if-let [customer-id (authorize/create-test-subscription "biz@harlanji.com")] ;; POST accepts only create. Perfect. 
+  ;  
+  ;    {:body (str "ok. " customer-id)}
+  ;    {:body "error."}))) 
+      
+      
+  ;(POST "/subscribe" [] (fn [req]
+  ;  (if-let [subscription-id (let [customer-profile-id "1920683905" ;"M_biz@harlanji.com"
+  ;                                 payment-profile-id "1833666257"
+  ;                                 amount 999.0]
+  ;                             (authorize/create-test-recurring-subscription-request
+  ;                               customer-profile-id
+  ;                               payment-profile-id
+  ;                               amount))]
+  ;    {:body (str "ok.")}
+  ;    {:body "error."})))
+
+
+
+
+
+          
   (route/files "/")
   (route/not-found "Page not found"))
 
-(def handler (wrap-subdirectories routes))
+(def handler (-> routes
+                 (wrap-keyword-params)
+                 (wrap-params)
+                 (wrap-subdirectories)))
